ef6c764afa0259a3540f9842f316ca40f060d971
Adrien Lacquemant <verblodung@gmail.com>
2017-10-03 15:39:03 -0400

    our final version

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+const mapboxgl = require('mapbox-gl');
+const buildMarker = require('./marker');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWxhcSIsImEiOiJjajhicnVxNGswMTlkMnFwbXdjbXNnbm9zIn0.rKQdLP-JkTSZjpLH0-sLTg';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  center: [-73.930000, 40.738913], // FullStack coordinates
+  zoom: 12, // starting zoom
+  style: 'mapbox://styles/mapbox/streets-v10' // mapbox has lots of different map styles available.
+});
+
+const marker = buildMarker('activity', [-74.009151, 40.705086]);
+marker.addTo(map);
